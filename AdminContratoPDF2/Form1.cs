946fc9d6e015e267ceb0ede3d1e2d903c65105fc
henryadasme <71049390+henryadasme@users.noreply.github.com>
2020-11-25 10:24:28 -0300

    Agregar archivos de proyecto.

---
+++ AdminContratoPDF2/Form1.cs
@@ -0,0 +1,41 @@
+﻿using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Element;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AdminContratoPDF2
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            crearPDF();
+        }
+
+        private void crearPDF()
+        {
+            PdfWriter pdfWriter = new PdfWriter("REPORTE.pdf");
+            PdfDocument pdf = new PdfDocument(pdfWriter);
+            Document documento = new Document(pdf, PageSize.LETTER);
+
+            documento.SetMargins(60, 20, 55, 20);
+            var parrafo = new Paragraph("nuevopdf");
+            documento.Add(parrafo);
+            documento.Close();
+        }
+    }
+}
